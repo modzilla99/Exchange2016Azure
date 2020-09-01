44090d16d1d209f9a5c9f47789a0bbf273049b1c
modzilla99 <support@m0dz1lla.de>
2020-09-01 15:49:45 +0200

    CleanUp

--- Optain-Exchange-Certs.ps1
+++ Optain-Exchange-Certs.ps1
@@ -1,19 +1,19 @@
 param (
 	[Parameter(Mandatory)]
-    [string]$uri,
+	[string]$uri,
 	[Parameter(Mandatory)]
 	[string]$destination,
 	
 	[Parameter(Mandatory)]
 	[String]$Domain,
 	[Parameter(Mandatory)]
-    [string]$SubscriptionID,
+	[string]$SubscriptionID,
 	[Parameter(Mandatory)]
 	[string]$TenantId,
 	[Parameter(Mandatory)]
-    [string]$AppID,
+	[string]$AppID,
 	[Parameter(Mandatory)]
-    [string]$AZAppPass
+	[string]$AZAppPass
 )
 
 function Get-LECertificates {
@@ -24,9 +24,9 @@ function Get-LECertificates {
 	Install-Module -Name Posh-ACME -Force -Scope AllUsers
 
 	$azParams = @{
-		AZSubscriptionId = $SubscriptionID
-		AZTenantId = $TenantId
-		AZAppUsername = $AppID
+		AZSubscriptionId      = $SubscriptionID
+		AZTenantId            = $TenantId
+		AZAppUsername         = $AppID
 		AZAppPasswordInsecure = $AZAppPass
 	}
 
@@ -34,23 +34,23 @@ function Get-LECertificates {
 
 	New-Item -Path C:\Certificates -ItemType Directory -Force 
 	$Path = (Get-PACertificate).CertFile  
-	$Path = $Path.Substring(0,$Path.Length - 9) 
+	$Path = $Path.Substring(0, $Path.Length - 9) 
 	$Path = "$Path\*.*" 
 	Copy-Item -Path $Path -Destination C:\Certificates -Recurse 
 
 	
 }
 
-function DownloadISO {
+function Get-ExchangeISO {
 
 	# Local file storage location
-    $localPath = "$env:SystemDrive"
+	$localPath = "$env:SystemDrive"
 
-    # Log file
-    $logFileName = "CSDownload.log"
-    $logFilePath = "$localPath\$logFileName"
+	# Log file
+	$logFileName = "CSDownload.log"
+	$logFilePath = "$localPath\$logFileName"
 	
-	if(Test-Path $destination) {
+	if (Test-Path $destination) {
 		"Destination path exists. Skipping ISO download" | Tee-Object -FilePath $logFilePath -Append
 		return
 	}
@@ -59,13 +59,12 @@ function DownloadISO {
 	New-Item -Path $destination -ItemType Directory
 
 	$destinationFile = $null
-    $result = $false
+	$result = $false
 	# Download ISO
 	$retries = 3
 	# Stop retrying after download succeeds or all retries attempted
-	while(($retries -gt 0) -and ($result -eq $false)) {
-		try
-		{
+	while (($retries -gt 0) -and ($result -eq $false)) {
+		try {
 			"Downloading ISO from URI: $uri to destination: $destination" | Tee-Object -FilePath $logFilePath -Append
 			$isoFileName = [System.IO.Path]::GetFileName($uri)
 			$webClient = New-Object System.Net.WebClient
@@ -73,7 +72,8 @@ function DownloadISO {
 			$destinationFile = "$destination\$isoFileName"
 			$webClient.DownloadFile($uri, $destinationFile)
 			$_date = Get-Date -Format hh:mmtt
-			if((Test-Path $destinationFile) -eq $true) {
+			
+			if ((Test-Path $destinationFile) -eq $true) {
 				"Downloading ISO file succeeded at $_date" | Tee-Object -FilePath $logFilePath -Append
 				$result = $true
 			}
@@ -81,68 +81,71 @@ function DownloadISO {
 				"Downloading ISO file failed at $_date" | Tee-Object -FilePath $logFilePath -Append
 				$result = $false
 			}
-		} catch [Exception] {
+		}
+		catch [Exception] {
 			"Failed to download ISO. Exception: $_" | Tee-Object -FilePath $logFilePath -Append
 			$retries--
-			if($retries -eq 0) {
+			if ($retries -eq 0) {
 				Remove-Item $destination -Force -Confirm:0 -ErrorAction SilentlyContinue
 			}
 		}
 	}
 	
 	# Extract ISO
-	if($result)
-    {
-        "Mount the image from $destinationFile" | Tee-Object -FilePath $logFilePath -Append
-        $image = Mount-DiskImage -ImagePath $destinationFile -PassThru
-        $driveLetter = ($image | Get-Volume).DriveLetter
+	if ($result) {
+		"Mount the image from $destinationFile" | Tee-Object -FilePath $logFilePath -Append
+		$image = Mount-DiskImage -ImagePath $destinationFile -PassThru
+		$driveLetter = ($image | Get-Volume).DriveLetter
 
-        "Copy files to destination directory: $destination" | Tee-Object -FilePath $logFilePath -Append
-        Robocopy.exe ("{0}:" -f $driveLetter) $destination /E | Out-Null
+		"Copy files to destination directory: $destination" | Tee-Object -FilePath $logFilePath -Append
+		Robocopy.exe ("{0}:" -f $driveLetter) $destination /E | Out-Null
     
-        "Dismount the image from $destinationFile" | Tee-Object -FilePath $logFilePath -Append
-        Dismount-DiskImage -ImagePath $destinationFile
+		"Dismount the image from $destinationFile" | Tee-Object -FilePath $logFilePath -Append
+		Dismount-DiskImage -ImagePath $destinationFile
     
-        "Delete the temp file: $destinationFile" | Tee-Object -FilePath $logFilePath -Append
-        Remove-Item -Path $destinationFile -Force
-    }
-    else
-    {
+		"Delete the temp file: $destinationFile" | Tee-Object -FilePath $logFilePath -Append
+		Remove-Item -Path $destinationFile -Force
+	}
+	else {
 		"Failed to download the file after exhaust retry limit" | Tee-Object -FilePath $logFilePath -Append
 		Remove-Item $destination -Force -Confirm:0 -ErrorAction SilentlyContinue
-        Throw "Failed to download the file after exhaust retry limit"
-    }
+		Throw "Failed to download the file after exhaust retry limit"
+	}
 }
 
-Get-LECertificates
+function Get-BasicSoftware {
 
-DownloadISO
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+	Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	Start-Sleep 10
+	choco install "netfx-4.8" -y
+	choco install vscode -y
+	choco install microsoft-edge -y
+}
 
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
- iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
- sleep 10
- choco install "netfx-4.8" -y
- choco install vscode -y
- choco install microsoft-edge -y
- 
- 
- 
-$LanguageSetting = "de-DE"
-$GeoIDSetting = "94"
-$TimeZoneSetting = "W. Europe Standard Time"
-$regionalsettingsURL = "https://raw.githubusercontent.com/sredlin/Azure/master/AzureVMRegionalSettings/Settings/AzureVMLanguageDE.xml"
-$RegionalSettings = "D:\AzureVMLanguageDE.xml"
+function Set-RegionalSettings {
+	$LanguageSetting = "de-DE"
+	$GeoIDSetting = "94"
+	$TimeZoneSetting = "W. Europe Standard Time"
+	$regionalsettingsURL = "https://raw.githubusercontent.com/sredlin/Azure/master/AzureVMRegionalSettings/Settings/AzureVMLanguageDE.xml"
+	$RegionalSettings = "D:\AzureVMLanguageDE.xml"
        
-#downdload regional settings file
-Invoke-WebRequest $regionalsettingsURL -OutFile $RegionalSettings
+	#downdload regional settings file
+	Invoke-WebRequest $regionalsettingsURL -OutFile $RegionalSettings
 
         
-# Set Locale, language etc. 
-& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
+	# Set Locale, language etc. 
+	& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
 
-# Set languages/culture. Not needed perse.
-Set-WinSystemLocale $LanguageSetting
-Set-WinUserLanguageList -LanguageList $LanguageSetting -Force
-Set-Culture -CultureInfo $LanguageSetting
-Set-WinHomeLocation -GeoId $GeoIDSetting
-Set-TimeZone -Name $TimeZoneSetting
+	# Set languages/culture. Not needed perse.
+	Set-WinSystemLocale $LanguageSetting
+	Set-WinUserLanguageList -LanguageList $LanguageSetting -Force
+	Set-Culture -CultureInfo $LanguageSetting
+	Set-WinHomeLocation -GeoId $GeoIDSetting
+	Set-TimeZone -Name $TimeZoneSetting
+}
+
+	Get-LECertificates
+	Get-ExchangeISO
+	Get-BasicSoftware
+	Set-RegionalSettings
